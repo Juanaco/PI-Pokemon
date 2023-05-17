37ad9e6ed33fffb5c3c3b6c747e63a41fdc46c0b
Juanaco <gjuan.imhoff@gmail.com>
2023-05-17 09:52:19 -0300

    busqueda por name arreglada, modelo type con datos Api hecho, relacion de modelos, FRONT: views básicas, Store con initial state

--- api/src/controllers/pokemonsController.js
+++ api/src/controllers/pokemonsController.js
@@ -1,121 +1,134 @@
-const {Pokemon} = require("../db");
+const { Pokemon } = require("../db");
 const axios = require("axios");
 
-
-
-async function getPokemonsApi() 
-{
-    let arrayPokemonsApi = [];
-
-      // carga de pokeAPI -----------------------------------------
-    await axios.get("https://pokeapi.co/api/v2/pokemon")
-      .then(async (response) => {
-          let arrayResultApi = response.data.results;
-          let arrayPromises = [];
-          arrayResultApi.map((p) => arrayPromises.push(axios.get(p.url))); 
-          // se obtiene uno por uno los datos de cada pokemon
-        
-          await Promise.all(arrayPromises)
-          .then((pokemons) => {
-              arrayPokemonsApi = pokemons.map((p) => {
-                  return {
-                      id: p.data.id,
-                      name: p.data.name,
-                      image: p.data.sprites.other.dream_world.front_default,  // url imagen
-                      hp: p.data.stats[0].base_stat,
-                      attack: p.data.stats[1].base_stat,
-                      defense: p.data.stats[2].base_stat,
-                      speed: p.data.stats[3].base_stat,
-                      height: p.data.height,
-                      weight: p.data.weight,
-                      types: p.data.types.map((t) => {
-                          return {
-                              name: t.type.name
-                          }
-                      })
-                  };  // return 
-              }); // map
-          }) 
-          .catch((error) => {
-              return error;
-          });
-
-      })
-      .catch((error) => {
-          return error;
-      });
-        // ------------------------------- end - carga de poke API
-    return arrayPokemonsApi;
-};
-
-const getAllPokemon = async () =>{
+const construirPokemon = (pokemon) => {
     
-    const databasePokemon = await Pokemon.findAll();
-    
-    const apiPokemons = await getPokemonsApi();
-    
+}
 
-    return [...databasePokemon, ...apiPokemons];
-
-
-};
-
-const pokemonName = async (name) =>{
-    
-    const apiPoke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    const pokeArray = [];
-    pokeArray.push(apiPoke)
-    const filteredArray = pokeArray.map((p) => {
+const mapPokemons = (pokemons) => {
+  return pokemons.map(
+    // Extraer esta funcion a una nueva
+    (pokemon) => {
         return {
-            id: p.data.id,
-            name: p.data.name,
-            image: p.data.sprites.other.dream_world.front_default,  // url imagen
-            hp: p.data.stats[0].base_stat,
-            attack: p.data.stats[1].base_stat,
-            defense: p.data.stats[2].base_stat,
-            speed: p.data.stats[3].base_stat,
-            height: p.data.height,
-            weight: p.data.weight,
-            types: p.data.types.map((t) => {
-                    return {
-                        name: t.type.name
-                    }
-            })
-        }
-    });
-    return filteredArray; 
-}
-
-const searchPokemonsByName = async (name) =>{
-    
-    const apiPokemons = await pokemonName(name);
-    // ***********************************************
-    // apiPokemons rompe la búsqueda si busco un nombre de la BDD que no esté en la API 
-    const databasePokemon = await Pokemon.findAll({where:{ name }});
-        
-       
-    return [...databasePokemon, ...apiPokemons]
+        id: pokemon.data.id,
+        name: pokemon.data.name,
+        image: pokemon.data.sprites.other.dream_world.front_default, // url imagen
+        hp: pokemon.data.stats[0].base_stat,
+        attack: pokemon.data.stats[1].base_stat,
+        defense: pokemon.data.stats[2].base_stat,
+        speed: pokemon.data.stats[3].base_stat,
+        height: pokemon.data.height,
+        weight: pokemon.data.weight,
+        types: pokemon.data.types.map((t) => {
+            return {
+            name: t.type.name,
+            };
+        }),
+        }; // return
+    }
+  ); // map
 };
 
-const createPokemon = async (name, image, hp, attack, defense, speed, height, weight) =>
-// ************************************
-// Faltan los datos del modelo TYPE en el formulario 
-// ****************************************
-    await Pokemon.create({name, image, hp, attack, defense, speed, height, weight});
+async function getPokemonsApi() {
+  let arrayPokemonsApi = [];
 
+  // carga de pokeAPI -----------------------------------------
+  await axios
+    .get("https://pokeapi.co/api/v2/pokemon")
+    .then(async (response) => {
+      let arrayResultApi = response.data.results;
+      let arrayPromises = [];
+      arrayResultApi.map((p) => arrayPromises.push(axios.get(p.url)));
+      // se obtiene uno por uno los datos de cada pokemon
 
-
-    // ********************
-    // hay que filtrar los resultados , cómo getAllpokemon
-const getPokemonById = async (id, source) =>{
-    const pok = source === "api" ? 
-    (await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)).data
-    : await Pokemon.findByPk(id);
-    console.log(pok)
-
-    return pok;
+      await Promise.all(arrayPromises)
+        .then((pokemons) => {
+          arrayPokemonsApi = mapPokemons(pokemons);
+        })
+        .catch((error) => {
+          return error;
+        });
+    })
+    .catch((error) => {
+      return error;
+    });
+  // ------------------------------- end - carga de poke API
+  return arrayPokemonsApi;
 }
 
+const getAllPokemon = async () => {
+  const databasePokemon = await Pokemon.findAll();
 
+  const apiPokemons = await getPokemonsApi();
 
-module.exports= {createPokemon, getPokemonById, searchPokemonsByName, getAllPokemon};
+  return [...databasePokemon, ...apiPokemons];
+};
+
+const pokemonName = async (name) => {
+  try {
+    const apiPoke = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${name}`
+    );
+
+    const pokeArray = [];
+    pokeArray.push(apiPoke);
+
+    const filteredArray = mapPokemons(pokeArray);
+
+    return filteredArray;
+  } catch (error) {
+    return [];
+  }
+};
+
+const searchPokemonsByName = async (name) => {
+  const apiPokemons = await pokemonName(name);
+  // ***********************************************
+  // apiPokemons rompe la búsqueda si busco un nombre de la BDD que no esté en la API
+  const databasePokemon = await Pokemon.findAll({ where: { name } });
+
+  return [...databasePokemon, ...apiPokemons];
+};
+
+const createPokemon = async (
+  name,
+  image,
+  hp,
+  attack,
+  defense,
+  speed,
+  height,
+  weight
+) =>
+  // ************************************
+  // Faltan los datos del modelo TYPE en el formulario
+  // ****************************************
+  await Pokemon.create({
+    name,
+    image,
+    hp,
+    attack,
+    defense,
+    speed,
+    height,
+    weight,
+  });
+
+// ********************
+// hay que filtrar los resultados , cómo getAllpokemon
+const getPokemonById = async (id, source) => {
+  // Convertir en IF POSTA POSTA no este formato de mierda (ternario)
+  const pok =
+    source === "api"
+      ? (await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)).data
+      : await Pokemon.findByPk(id);
+
+  return pok;
+};
+
+module.exports = {
+  createPokemon,
+  getPokemonById,
+  searchPokemonsByName,
+  getAllPokemon,
+};
--- client/src/components/CardsConteinter/CardsConteiner.jsx
+++ client/src/components/CardsConteinter/CardsConteiner.jsx
@@ -1,93 +1,13 @@
+import { useSelector } from "react-redux";
 import Card from "../Card/Card";
 import style from "./CardsContainer.module.css";
 
+
+
 const CardsContainer = () => {
-    const pokemons = [
-        {
-            "id": "4949a4d5-735d-48cb-90d0-5b93dd25ea80",
-            "name": "largando baranda",
-            "image": "img",
-            "hp": 30,
-            "attack": 40,
-            "defense": 14,
-            "speed": 400,
-            "height": 12,
-            "weight": 4,
-            "created": true
-        },
-        {
-            "id": "4892f045-866a-4159-9c5c-c13aa40cf65e",
-            "name": "pikachu de marihuana",
-            "image": "img",
-            "hp": 30,
-            "attack": 40,
-            "defense": 14,
-            "speed": 400,
-            "height": 12,
-            "weight": 4,
-            "created": true
-        },
-        {
-            "id": "91bcae4d-9cd4-4525-839e-19a91a974cdf",
-            "name": "pikachu",
-            "image": "img",
-            "hp": 30,
-            "attack": 40,
-            "defense": 14,
-            "speed": 400,
-            "height": 12,
-            "weight": 4,
-            "created": true
-        },
-        {
-            "id": "1caf37b9-2eca-46c4-8715-a0ba8efb5a78",
-            "name": "bulba",
-            "image": "img",
-            "hp": 30,
-            "attack": 40,
-            "defense": 14,
-            "speed": 400,
-            "height": 12,
-            "weight": 4,
-            "created": true
-        },
-        {
-            "id": "ac679f3b-f342-4a76-b175-6026351e3a05",
-            "name": "bulbasaur",
-            "image": "img",
-            "hp": 30,
-            "attack": 40,
-            "defense": 14,
-            "speed": 400,
-            "height": 12,
-            "weight": 4,
-            "created": true
-        },
-        {
-            "id": "5a0aaa89-d6a7-422b-ac2e-a4919870ef63",
-            "name": "asuca",
-            "image": "img",
-            "hp": 30,
-            "attack": 40,
-            "defense": 14,
-            "speed": 400,
-            "height": 12,
-            "weight": 4,
-            "created": true
-        },
-        {
-            "id": "2dd9d40e-86e9-406d-8f21-f98b6ed88fb9",
-            "name": "ivysaur",
-            "image": "img",
-            "hp": 30,
-            "attack": 40,
-            "defense": 14,
-            "speed": 400,
-            "height": 12,
-            "weight": 4,
-            "created": true
-        }
-    ]
+
+    const pokemons = useSelector(state=>state.pokemons);
+   
     return(
         <div className={style.container}>
             {pokemons.map(pokemon=>{
